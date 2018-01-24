8e56eaa319d048962283d764dfdcc96694dae835
Chris Watson <chris@watzon.me>
2018-01-24 12:14:49 -0700

    Add some more logs

--- src/commands/referredBy.js
+++ src/commands/referredBy.js
@@ -19,14 +19,17 @@ module.exports = (bot) => bot.command('referredby', async (ctx) => {
 
   // Make sure the referral code matches a user
   if (!referredByUser)
+    logger.info('A user with that referral code doesn\'t exist')
     return ctx.reply(`A user with that referral code doesn't exist. Are you sure you got it right?`)
 
   // Keep people from using their own referral code
   if (referredUser.id == referredByUser.id)
+    logger.info('Attempted to use their own code')
     return ctx.reply(`Sorry, but you can't use your own referral code.`)
 
   // Make sure they don't try to add another referral
   if (referredUser.referred_by)
+    logger.info('Already referred')
     return ctx.reply('You can only be referred by one user.')
 
   referredUser.referred_by = referredByUser
@@ -34,5 +37,6 @@ module.exports = (bot) => bot.command('referredby', async (ctx) => {
 
   await referredByUser.incrementReferralCount()
 
+  logger.info(`@${referredByUser.username} successfully referred @${referredUser.username}`)
   ctx.reply(`Thank you @${referredByUser.username} for referring @${referredUser.username}`)
 })
