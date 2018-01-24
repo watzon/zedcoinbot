e58d5dc5d2054f009e9e59e72f67df5e1a7cf239
Chris Watson <chris@watzon.me>
2018-01-23 18:27:11 -0700

    Updated referral message

--- src/commands/referralCode.js
+++ src/commands/referralCode.js
@@ -5,5 +5,5 @@ module.exports = (bot) => bot.command('referralcode', async (ctx) => {
   const user = await User.findOne({ tgid: tguser.id })
 
   const referralCode = user.referral_code
-  ctx.reply(`@${tguser.username} your referral code is ${referralCode}\n\nSPREAD THE ZED! 🎉`)
+  ctx.reply(`@${tguser.username} your referral code is ${referralCode}\n\nUse it to refer your friends to @ZEDNetworkInc! 🎉`)
 })
